6a05c542ed11631d185d36d05c681fe73f0032ad
BHN16 <34187868+BHN16@users.noreply.github.com>
2019-09-17 07:54:16 -0500

    Fix(Node.h):Ready to make inheritance

--- Node.h
+++ Node.h
@@ -1,18 +1,37 @@
-#ifndef LINKED_LISTS_NODE_H
-#define LINKED_LISTS_NODE_H
-
-template<typename T>
+#ifndef ALGORITMOS_NODE_H
+#define ALGORITMOS_NODE_H
+template<class T>
 class Node{
 protected:
-    T value;
+    T Value;
 public:
-    Node* Next;
-    Node(T value);
-    Node(T value, Node* N);
+    Node();
+    Node(T v);
     T get_value();
-    template <typename _T>
+    template<class R>
+    friend class List;
+
+    template<class Y>
     friend class ForwardList;
+
+    template<class U>
+    friend class CircularList;
+
+    template<class H>
+    friend class DoubleCircularList;
 };
 
+template<class T>
+Node<T>::Node(){
+}
 
-#endif
+template<class T>
+Node<T>::Node(T v){
+    this->Value = v;
+}
+
+template<class T>
+T Node<T>::get_value(){
+    return (this->Value);
+}
+#endif //ALGORITMOS_NODE_H
